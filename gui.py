ce96f4823ded5f4d1334db471a4dbdd5aca7d416
pietrō <70135818+shogun-mar@users.noreply.github.com>
2024-07-26 13:05:16 +0200

    continue langton simulation pt2

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 import pygame
 from collections import deque
-from random import choice, randrange
+from random import randrange
 
 class Ant:
     def __init__(self, app, pos, color):
@@ -17,35 +17,37 @@ class Ant:
             SIZE = self.app.self.CELL_SIZE
             rect = self.x * SIZE, self.y * SIZE, max(SIZE - 1, 1), max(SIZE - 1, 1)
             
-            pygame.draw.rect(self.app.screen, self.color, rect)
+            if value:
+                pygame.draw.rect(self.app.screen, 'white', rect)
+            else:
+                pygame.draw.rect(self.app.screen, self.color, rect)
 
             self.increments.rotate(1) if value else self.increments.rotate(-1)
             dx, dy = self.increments[0]
             self.x = (self.x + dx) % self.app.COLS
             self.y = (self.y + dy) % self.app.ROWS
 
-    @staticmethod
-    def get_color():
-        channel = lambda: randrange(30, 220) #Lambda function that returns a random number between 30 and 220
-        return channel(), channel(), channel()
-
 class App:
-    def __init__(self, CELL_SIZE = 16, NUM_ANTS = 400):
+    def __init__(self, CELL_SIZE = 16):
         pygame.init()
         self.CELL_SIZE = CELL_SIZE
+        self.ROWS = self.COLS = 16
         self.WIDTH, self.HEIGTH = 1200, 900
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGTH))
         pygame.display.set_icon(pygame.image.load('ant_icon.png'))
         self.clock = pygame.time.Clock()
-        self.grid = [[0 for col in range(16)] for row in range(16)]
+        self.grid = [[0 for col in range(self.COLS)] for row in range(self.COLS)]
 
         self.colors = set('greenyellow goldenrod2 fuchsia deeppink4 cornflowerblue darkred darkslategrey blueviolet mediumpurple3 indigo crimson seagreen1 salmon1 purple2 plum3 palevioletred violetred springgreen4 \
                           steelblue4 steelblue slateblue4 slateblue sienna2 cyan skyblue purple magenta red green blue yellow \
                           white'.split())
         
+        self.ants = []
+
         self.is_placement_phase: bool = True
         self.placement_phase_grid = pygame.image.load('selection_phase_grid.png')
         self.placement_phase_grid_rect = self.placement_phase_grid.get_rect(center = (self.WIDTH // 2, self.HEIGTH // 2))
+        self.placement_grid_offset = self.placement_phase_grid_rect.topleft
 
         self.title_font = pygame.font.Font('mexcellent/mexcellent 3d.otf', 64)
         self.title_text = self.title_font.render('Langton\'s Ant Simulation', True, 'white')
@@ -53,7 +55,7 @@ class App:
         self.screen.blit(self.title_text, self.title_rect)
 
         self.counter_font = pygame.font.Font('mexcellent/mexcellent 3d.otf', 48)
-        self.ant_counter_text = self.counter_font.render(f'Ants: {NUM_ANTS}', True, 'white')
+        self.ant_counter_text = self.counter_font.render('Ants: 0', True, 'white')
         self.ant_counter_rect = self.ant_counter_text.get_rect(center = (225, 300))
         self.screen.blit(self.ant_counter_text, self.ant_counter_rect)
 
@@ -80,27 +82,59 @@ class App:
                     if event.key == pygame.K_SPACE and self.is_placement_phase:
                         self.is_placement_phase = False
                 if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and self.is_placement_phase:
-                    if self.start_button_rect.collidepoint(event.pos):
+                    if self.start_button_rect.collidepoint(event.pos) and self.is_simulation_ready():
                         self.is_placement_phase = False
                         self.start_button_text = self.button_font.render('Start', True, 'white')
                         self.screen.blit(self.start_button_text, self.start_button_rect)
+                    elif self.placement_phase_grid_rect.collidepoint(event.pos):
+                        relative_mouse_pos = event.pos[0] - self.placement_grid_offset[0], event.pos[1] - self.placement_grid_offset[1]
+                        x, y = relative_mouse_pos
+                        column, row = x // self.CELL_SIZE, y // self.CELL_SIZE
+                        self.ants.append(Ant(self, (column, row), 'white'))
+                        self.grid[column][row] = 1
             
             pygame.display.set_caption(f'Langton\'s Ant Simulation - FPS: {int(self.clock.get_fps())}')
         
             if self.is_placement_phase: 
                 self.screen.blit(self.placement_phase_grid, self.placement_phase_grid_rect)
+
+                coverup_surf = pygame.Surface(self.ant_counter_text.get_size())
+                self.screen.blit(coverup_surf, self.ant_counter_rect)
+                num_ants = len(self.ants)
+                self.ant_counter_text = self.counter_font.render(f'Ants: {num_ants}', True, 'white')
+                self.ant_counter_rect.center = (225 - 15 * len(str(num_ants)), 300)
+                self.screen.blit(self.ant_counter_text, self.ant_counter_rect)
+
+                self.draw_already_placed_ants()
             else:
-                self.white_cell_counter_text = self.counter_font.render(f'White Cells:', True, 'white')
+                coverup_surf = pygame.Surface(self.white_cell_counter_text.get_size())
+                self.screen.blit(coverup_surf, self.white_cell_counter_rect)
+                white_cells_num = self.get_white_cells()
+                self.white_cell_counter_text = self.counter_font.render(f'White Cells: {white_cells_num}', True, 'white')
+                self.white_cell_counter_rect.center = (self.WIDTH - 225 - 15 * len(str(white_cells_num)), 300)
                 self.screen.blit(self.white_cell_counter_text, self.white_cell_counter_rect)
 
-            #[ant.run() for ant in self.ants]
+                coverup_surf = pygame.Surface(self.grey_cell_counter_text.get_size())
+                self.screen.blit(coverup_surf, self.grey_cell_counter_rect)
+                grey_cells_num = self.ROWS * self.COLS - white_cells_num
+                self.grey_cell_counter_text = self.counter_font.render(f'Grey Cells: {grey_cells_num}', True, 'white')
+                self.grey_cell_counter_rect.center = (self.WIDTH - 225 - 15 * len(str(grey_cells_num)), 550)
+                self.screen.blit(self.grey_cell_counter_text, self.grey_cell_counter_rect)
+
+                #[ant.run() for ant in self.ants]
 
             pygame.display.flip()
-            self.clock.tick()
+            self.clock.tick(60)
 
     def get_white_cells(self):
-        return sum([sum(row) for row in self.grid])
-
+        return sum(cell for row in self.grid for cell in row)
+    
+    def is_simulation_ready(self):
+        return len(self.ants) > 0
+    
+    def draw_already_placed_ants(self):
+        grid_origin_x, grid_origin_y = self.placement_grid_offset
+    
 if __name__ == "__main__":
     app = App()
     app.run()
